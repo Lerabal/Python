8cd5bbdf81f7a43ccc026f77e228038b6772a55c
lera <lera_bal@list.ru>
2023-03-21 10:49:43 +0300

    16_18_20

--- DZ_3/task16.py
+++ DZ_3/task16.py
@@ -14,3 +14,27 @@ for i in range(n):
     if list_1[i] == x:
         count += 1
 print(count)
+
+# n=int(input("ВВедите число элементов массива = "))
+# x=int(input("ВВедите число X = "))
+# #list=[int(input(f"Введите {i} элемент массива ")) for i in range(n)]
+# #print(list)
+# list=[]
+# counter=0
+# for i in range(n):
+# list.append(int(input(f"Введите {i} элемент массива ")))
+# if x ==list[i]:
+# counter+=1
+# print(list)
+# print(f"Число {x} встречается в массиве {counter} раз/а")
+# list2=list.copy()
+# counter2=0
+# x_str = str(x)
+# for num in list2:
+# num_str = str(num)
+# if x_str in num_str:
+# # print(number)
+# for elem in num_str:
+# if x_str == elem:
+# counter2+=1
+# print(f"Цифра {x} встречается в массиве {counter2} раз/а")
--- DZ_3/task18.py
+++ DZ_3/task18.py
@@ -12,3 +12,23 @@ list_1 = [randint(1, 48) for _ in range(n)]
 print(list_1)
 list_2=[abs(list_1[i]-x) for i in range(len(list_1))]
 print(list_1[list_2.index(min(list_2))])
+
+# n = int(input("ВВедите число элементов массива = "))
+# x = int(input("ВВедите число X = "))
+# list = [int(input(f"Введите {i} элемент массива ")) for i in range(n)]
+# print(list)
+# uniq = set()
+# elem_min_diff = list[0]
+# min_diff = abs(list[0]-x)
+# for num in list[1:]:
+# cur_diff = abs(num-x)
+# if cur_diff < min_diff and num != x:
+# elem_min_diff = num
+# min_diff = cur_diff
+# uniq.clear()
+# uniq.add(elem_min_diff)
+# elif cur_diff == min_diff and num != x:
+# uniq.add(num)
+
+# print(f"Самые близкие числa к {x} :")
+# print(*uniq)
