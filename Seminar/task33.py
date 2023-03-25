79f4bd5b7f56749103b1fc46f8a27a510e5b3237
lera <lera_bal@list.ru>
2023-03-25 21:30:13 +0400

    33

--- Seminar/task33.py
+++ Seminar/task33.py
@@ -23,3 +23,12 @@ def minmax (array):
 
 arraynew=magazin(10)
 print(minmax(arraynew))
+
+# def min_and_max(iterable):
+# min_value, max_value = float('inf'), float('-inf')
+# for value in iterable:
+# if value < min_value:
+# min_value = value
+# if value > max_value:
+# max_value = value
+# return min_value, max_value
